157e58cfba7dbbb38b1f774c5645311ec6ce96b5
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-01-12 18:44:02 +0100

    add screenshot, clean up protocol

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,16 +12,14 @@ void Player::setPosition(int x) {
   this->x = x;
 }
 
-void Player::send(unsigned char* buf, int startPos) const
-{
+void Player::send(unsigned char* buf, int startPos) const {
   buf[startPos + 0] = ((unsigned char*)&x)[0];
   buf[startPos + 1] = ((unsigned char*)&x)[1];
   buf[startPos + 2] = ((unsigned char*)&x)[2];
   buf[startPos + 3] = ((unsigned char*)&x)[3];
 }
 
-void Player::receive(unsigned char* buf, int startPos)
-{
+void Player::receive(unsigned char* buf, int startPos) {
   ((unsigned char*)&x)[0] = buf[startPos + 0];
   ((unsigned char*)&x)[1] = buf[startPos + 1];
   ((unsigned char*)&x)[2] = buf[startPos + 2];
--- src/protocol.h
+++ src/protocol.h
@@ -3,23 +3,7 @@
 
 #include <string>
 
-const std::string port = "6060";
-
-
 const unsigned char BEGIN_MESSAGE = 0xAB;
-
-const unsigned char REQUEST_START = 0x10;
 const unsigned char REQUEST_END = 0x11;
-const unsigned char REQUEST_GAME_STATUS = 0x12;
-
-const unsigned char REQUEST_CURSOR_POSITION = 0x40;
-
-const unsigned char REQUEST_STATE = 0x50;
-
-const unsigned char REQUEST_RECEIVE_PLAYER0 = 0x80;
-const unsigned char REQUEST_RECEIVE_PLAYER1 = 0x81;
-const unsigned char REQUEST_RECEIVE_BALL = 0x82;
-const unsigned char REQUEST_RECEIVE_CURSOR_POSITION = 0x84;
-
 
 #endif // PROTOCOL_H
